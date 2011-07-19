9352aa685153e5c0933a0f17f68791198881c3c7
Noah Heldman <noah.heldman@gmail.com>
2011-07-18 23:04:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freshbooks::Application.config.secret_token = '7c199f599b0978c6eb4e7316cdfe3a62486fb533e51d29f87ea72ca70c792752b0e4dd24ea6145ca9ce27bb746bd1c03536bd509b369abc00ad5d968000c3c64'
